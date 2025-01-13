6ae2027064d260d890a10e004f4194aa80fcf3a1
Odyseus2 <64859072+Odyseus2@users.noreply.github.com>
2025-01-13 09:07:09 +0100

    Rename cases

--- src/main/org/centrale/medev/tpnote/Plateau.java
+++ src/main/org/centrale/medev/tpnote/Plateau.java
@@ -1,24 +1,25 @@
 package org.centrale.medev.tpnote;
 
 import java.util.Optional;
+import org.centrale.medev.tpnote.Pion;
 
 /**
  * Plateau de jeu de 8 cases par 8 cases
  */
 public class Plateau {
-    private Pion[][] Cases = new Pion[8][8];
+    private Pion[][] cases = new Pion[8][8];
 
     /**
      * Constructeur
      */
     public Plateau() {
-        Cases[3][3] = new Pion(false);
-        Cases[4][4] = new Pion(false);
-        Cases[3][4] = new Pion(true);
-        Cases[4][3] = new Pion(true);
+        cases[3][3] = new Pion(false);
+        cases[4][4] = new Pion(false);
+        cases[3][4] = new Pion(true);
+        cases[4][3] = new Pion(true);
     }
 
-    public Optional[Pion] getCase(int x, int y){
-        return Cases[x][y];
+    public Optional<Pion> getCase(int x, int y){
+        return cases[x][y];
     }
 }
