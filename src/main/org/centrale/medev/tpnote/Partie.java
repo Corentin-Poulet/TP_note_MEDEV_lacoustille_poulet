9707e987fbd18d3551090beb699a04bac8f41ba6
Odyseus2 <64859072+Odyseus2@users.noreply.github.com>
2025-01-13 10:20:24 +0100

    Actions pour le joueur

--- src/main/org/centrale/medev/tpnote/Partie.java
+++ src/main/org/centrale/medev/tpnote/Partie.java
@@ -1,5 +1,6 @@
 package org.centrale.medev.tpnote;
-
+import java.util.Scanner;
+import java.util.logging.Logger;
 public class Partie{
 	private Boolean joueurActif;
 	private Integer noTour;
@@ -21,9 +22,19 @@ public class Partie{
 	}
 
 	private void actionJoueur(boolean joueur){
-		
+		Scanner scan = new Scanner(System.in);
+		String action = scan.nextLine();
 	}
 
+	private void afficherMessage(boolean joueur){
+		Logger logger = Logger.getGlobal();
+		String couleur;
+		if (joueur) {
+			couleur = "blanc";
+		}else{couleur = "noir";}
+		logger.info("Vous jouez avec les"+couleur+"s");
+		logger.info("Choisissez une case : ");
+	}
 	public Boolean getJoueurActif(){
 		return joueurActif;
 	}
