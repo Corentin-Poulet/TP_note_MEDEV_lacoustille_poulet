a3a09a298b3f732243834060d0fecd77a089ba46
Odyseus2 <64859072+Odyseus2@users.noreply.github.com>
2025-01-13 09:57:05 +0100

    Actions de base de partie

--- src/main/org/centrale/medev/tpnote/Jeu.java
+++ src/main/org/centrale/medev/tpnote/Jeu.java
@@ -1,8 +1,7 @@
 package org.centrale.medev.tpnote;
 
 public class Jeu{
-	private partie;
 	public static void main(String[] args) {
-		partie = new Partie();
+		Partie partie = new Partie();
 	}
 }
--- src/main/org/centrale/medev/tpnote/Partie.java
+++ src/main/org/centrale/medev/tpnote/Partie.java
@@ -3,6 +3,30 @@ package org.centrale.medev.tpnote;
 public class Partie{
 	private Boolean joueurActif;
 	private Integer noTour;
-	private Plateau plateau = new Plateau;
-	
+	private Plateau plateau;
+	private Boolean partieEnCours;
+
+	Partie(){
+		plateau = new Plateau();
+		noTour = 0;
+		jouer();
+		partieEnCours = true;
+	}
+	private void jouer(){
+		while (partieEnCours){
+			noTour +=1;
+			joueurActif = false;
+			actionJoueur(joueurActif);
+		}
+	}
+
+	private void actionJoueur(boolean joueur){
+		
+	}
+
+	public Boolean getJoueurActif(){
+		return joueurActif;
+	}
+
+
 }
